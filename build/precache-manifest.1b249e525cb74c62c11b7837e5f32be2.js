77dba8c94aec0e2f1eaf4f54e2a5a096bb1ed248
chautruong79 <62908290+chautruong79@users.noreply.github.com>
2020-05-31 19:16:15 -0600

    Create precache-manifest.1b249e525cb74c62c11b7837e5f32be2.js

---
+++ build/precache-manifest.1b249e525cb74c62c11b7837e5f32be2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b7858c60e451a336dc62d9384b228e82",
+    "url": "/toDoList-v3/index.html"
+  },
+  {
+    "revision": "d0b10b6cf36c0074e538",
+    "url": "/toDoList-v3/static/js/2.69cf58b3.chunk.js"
+  },
+  {
+    "revision": "809f8196affeaac0e5176b72b79c267c",
+    "url": "/toDoList-v3/static/js/2.69cf58b3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f3f1cfcaaa3459e0f0dc",
+    "url": "/toDoList-v3/static/js/main.18f49b3a.chunk.js"
+  },
+  {
+    "revision": "e018f25e63491f14876f",
+    "url": "/toDoList-v3/static/js/runtime-main.bb45e5c3.js"
+  }
+]);
